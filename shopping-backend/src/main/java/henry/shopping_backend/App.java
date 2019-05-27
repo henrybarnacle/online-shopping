75a69d1e5f1024b3b2239bc6bbdcd87e7993529c
henrybarnacle <henrybarnacle@yahoo.com>
2019-05-27 16:13:30 -0500

    inital commit

---
+++ shopping-backend/src/main/java/henry/shopping_backend/App.java
@@ -0,0 +1,13 @@
+package henry.shopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
